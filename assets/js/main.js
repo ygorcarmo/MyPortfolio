584b2d9913de127320212afbcc4e06a739150ef5
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2021-10-18 18:25:56 +1100

    added aws config for contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,6 +56,65 @@ sr.reveal('.work__img',{interval: 200});
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200}); 
 
+function show(){
+
+	var name = document.getElementById("name").value;
+	// still need to add email config .indexOf("@") 
+	var email = document.getElementById("email").value;
+	var message = document.getElementById("message").value;
+	var api = "https://21c7bl605f.execute-api.us-east-1.amazonaws.com/Dev/contactus";
+
+	email.toString();
+	
+	// set parameters for dynamodb table
+	var params = {
+		"name":name,
+		"email":email,
+		"message":message,
+	};
+	
+	
+	// calling api function
+	var xhttp = new XMLHttpRequest();
+	
+	// when button pressed will run lambda function
+	xhttp.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200) {
+			// this will return lambda asnwer as an alert
+			alert(xhttp.responseText);
+		}
+	};
+
+	// email characters
+	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	// checking if email is correct
+	var checkemail = mailformat.test(email);
+	
+	// errors if wrong or no values are insert
+	if(name === "" || name === null){
+		alert("Please insert a name!");
+	}else if (!checkemail){
+		alert("Not a valid e-mail!");
+	}else if(message === "" || message === null){
+		alert("Please insert a nice comment!");
+
+	// this will run the serveles service
+	} else{
+		
+		// calling post method on api getway
+		xhttp.open("POST", api, false);
+		
+		// sending parameters to lambda 
+		var a = xhttp.send(JSON.stringify(params));
+		
+		// this will reset the input values once data submitted 
+		document.getElementById("name").value = '';
+		document.getElementById("email").value = '';
+		document.getElementById("message").value = '';
+
+	}
+}
+
 
 
 
